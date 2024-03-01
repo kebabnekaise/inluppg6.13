aadeadf47f99180647394118732f47f5f5630224
kebabnekaise <emsi06001@utb.vaxjo.se>
2024-03-01 10:56:11 +0100

    Add project files.

---
+++ inluppg6.13/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace inluppg6_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange en sträng med korten du har");
+            string inputString = Console.ReadLine();
+            string[] kortArray = inputString.Split(' ');
+            Console.WriteLine(Summera(kortArray));
+
+        }
+
+        static int Summera(string[] kortArray)
+        {
+            Dictionary<string, int> kort = new Dictionary<string, int>();
+            foreach(string str in  kortArray)
+            {
+                kort[str] = 0;
+            }
+            foreach(string str in kortArray)
+            {
+                if (kort[str] == 0)
+                {
+                    kort[str] = 1;
+                }
+                else
+                {
+                    kort[str]++;
+                }
+            }
+
+            int sum = 0;
+
+            foreach (string kortVal in kort.Keys)
+            {
+                sum += (int)Math.Pow(int.Parse(kortVal), kort[kortVal]);
+            }
+
+
+            return sum;
+        }
+    }
+}
